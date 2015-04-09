864090a7a1fc7193c3da0f2e562e792037d3f4a8
Mike <mjdryden@gmail.com>
2015-04-08 21:47:50 -0500

    initial solution setup

---
+++ src/Core/Models/BrewClubContext.cs
@@ -0,0 +1,26 @@
+namespace mbsoft.Models
+{
+	using System;
+	using System.Data.Entity;
+	using System.Linq;
+
+	public class BrewClubContext : DbContext
+	{
+		// Your context has been configured to use a 'BrewClubContext' connection string from your application's 
+		// configuration file (App.config or Web.config). By default, this connection string targets the 
+		// 'mbsoft.Models.BrewClubContext' database on your LocalDb instance. 
+		// 
+		// If you wish to target a different database and/or database provider, modify the 'BrewClubContext' 
+		// connection string in the application configuration file.
+		public BrewClubContext()
+			: base("name=BrewClubContext")
+		{
+		}
+
+		// Add a DbSet for each entity type that you want to include in your model. For more information 
+		// on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+
+		// public virtual DbSet<MyEntity> MyEntities { get; set; }
+	}
+
+}
---
+++ src/Website/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="mbsoft.BrewClub.Website.MvcApplication" Language="C#" %>
