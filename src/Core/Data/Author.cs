05383fa291bf6321f5838be30c36f47722d577db
Mike <pudds55@gmail.com>
2015-05-07 16:28:14 -0500

    added entities

---
+++ src/Core/Data/Author.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mbsoft.BrewClub.Data
+{
+	public class Author
+	{
+
+		public int AuthorId { get; set; }
+
+		public int AuthorName { get; set; }
+
+	}
+}
