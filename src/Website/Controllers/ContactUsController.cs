014a279bf84b2a1b5137f6b9fafa140bf255ae27
bradpowell <bradpowell78@gmail.com>
2015-05-10 21:44:29 -0500

    Initial dev for #22
    Added View and Controller for ContactUs Page.  This is pretty basic so
    there is no need for a model (I think).
    
    Updated Header's ContactUs anchor to point to new page.

---
+++ src/Website/Controllers/ContactUsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AttributeRouting.Web.Mvc;
+
+namespace mbsoft.BrewClub.Website.Controllers
+{
+    public class ContactUsController : Controller
+    {
+        [GET("ContactUs")]
+        public ActionResult ContactUs()
+        {
+            return View();
+        }
+    }
+}
